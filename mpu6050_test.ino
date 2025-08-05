c9026dcf9cf99a250eb4905d97c852557e40b8d5
Vijayaragavan004 <147318546+Vijayaragavan004@users.noreply.github.com>
2025-08-05 20:40:33 +0530

    Add files via upload

---
+++ mpu6050_test.ino
@@ -0,0 +1,42 @@
+#include <Wire.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+
+Adafruit_MPU6050 mpu;
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10);
+
+  // Try to initialize!
+  if (!mpu.begin())
+  {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1)
+    {
+      delay(10);
+    }
+  }
+
+  Serial.println("MPU6050 Found!");
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  delay(100);
+}
+
+void loop()
+{
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  /* Print out the values */
+  Serial.print("Accel X: "); Serial.print(a.acceleration.x); Serial.print(" m/s^2\t");
+  Serial.print("Y: "); Serial.print(a.acceleration.y); Serial.print("\t");
+  Serial.print("Z: "); Serial.print(a.acceleration.z); Serial.println("");
+
+  delay(500);
+}
